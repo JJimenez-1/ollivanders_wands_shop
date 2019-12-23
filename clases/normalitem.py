64ca1f1787ab30327a1f9c506d2b58d137dcac08
DanielFernandezR <dfernandez@cifpfbmoll.eu>
2019-12-23 19:49:52 +0100

    feat: Añadir algoritmo clase Normalitem
    Hemos añadido el algoritmo que debe tener la clase Normalitem

---
+++ clases/normalitem.py
@@ -0,0 +1,14 @@
+from updatable import Updatable
+from item import Item
+
+
+class Normalitem(Item, Updatable):
+
+    def set_sellin(self):
+        self.sell_in -= 1
+
+    def update_quality(self):
+        if self.quality <= 50 or self.quality > 0:
+            self.quality -= 1
+        else:
+            pass
