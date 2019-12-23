e2f054fc9440605ad4352c0ab45e949ef655fb62
DanielFernandezR <dfernandez@cifpfbmoll.eu>
2019-12-23 21:37:11 +0100

    fix: Cambiar error en normalitem y añadir caso test
    He arreglado un bug en la clase Normalitem y he añadido un caso test
    funcional para esa clase

--- clases/normalitem.py
+++ clases/normalitem.py
@@ -6,9 +6,13 @@ class Normalitem(Item, Updatable):
 
     def set_sellin(self):
         self.sell_in -= 1
+        normal_item = self.__repr__()
+        return normal_item
 
     def update_quality(self):
         if self.quality <= 50 or self.quality > 0:
             self.quality -= 1
         else:
             pass
+        normal_item = self.__repr__()
+        return normal_item
--- test_items.py
+++ test_items.py
@@ -3,5 +3,7 @@ from clases.normalitem import Normalitem
 from clases.updatable import Updatable
 
 
-def test_clase_funciona_correctamente():
-    assert Normalitem.set_sellin("potatoe", 10, 8) == ("potatoe", 9, 7)
+def test_class_normalitem_works():
+    normal_item = Normalitem("potatoe", 10, 8)
+    item_sellin_updated = normal_item.set_sellin() and normal_item.update_quality()
+    assert item_sellin_updated == "potatoe, 9, 7"
