28f2b8bd43780d26c6af720026f0e7ee2a606faf
JJimenez-1 <jjimenez@cifpfbmoll.eu>
2019-12-27 14:58:45 +0100

    feat: Crear clase backstagepass y caso test de esta

---
+++ clases/backstagepass.py
@@ -0,0 +1,17 @@
+from clases.normalitem import Normalitem
+
+
+class Backstagepass(Normalitem):
+
+    def update_quality(self):
+        if self.sell_in > 10:
+            self.quality += 1
+        elif self.sell_in <= 10 and self.sell_in > 5:
+            self.quality += 2
+        else:
+            self.quality += 3
+
+        if self.sell_in < 0:
+            self.quality == 0
+        backstage_pass = self.__repr__()
+        return backstage_pass
--- test_items.py
+++ test_items.py
@@ -3,6 +3,7 @@ from clases.normalitem import Normalitem
 from clases.updatable import Updatable
 from clases.agedbrie import Agedbrie
 from clases.sulfuras import Sulfuras
+from clases.backstagepass import Backstagepass
 
 def test_class_normalitem_works():
     normal_item = Normalitem("potatoe", 10, 8)
@@ -38,3 +39,9 @@ def test_class_Sulfuras_works():
     sulfuras = Sulfuras("Sulfuras", 0, 50)
     item_update = sulfuras.set_sellin() and sulfuras.update_quality()
     assert item_update == "Sulfuras, 0, 50"
+
+
+def test_class_Backstagepass_works():
+    backstage_pass = Backstagepass("Backstagepass", 3, 10)
+    item_update = backstage_pass.set_sellin() and backstage_pass.update_quality()
+    assert item_update == "Backstagepass, 2, 13"
