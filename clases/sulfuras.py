244046fb3cdb59b9168edaaa34539d5658799bc8
JJimenez-1 <jjimenez@cifpfbmoll.eu>
2019-12-27 14:01:46 +0100

    feat: Añadir funciones set_sellin y update_quality
    Hemos añadido las dos funciones set_sellin y update_quality, en Sulfuras

--- clases/sulfuras.py
+++ clases/sulfuras.py
@@ -1,4 +1,14 @@
 from clases.normalitem import Normalitem
 
+
 class Sulfuras(Normalitem):
-    
+
+    def set_sellin(self):
+        self.sell_in == self.sell_in
+        sulfuras = self.__repr__()
+        return sulfuras
+
+    def update_quality(self):
+        self.quality == self.quality
+        sulfuras = self.__repr__()
+        return sulfuras
