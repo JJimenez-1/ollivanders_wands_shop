46a3420b055d3f9062e0b20e2f345f756e7402c5
DanielFernandezR <dfernandez@cifpfbmoll.eu>
2020-01-05 18:19:40 +0100

    feat: Añadir barricada al añadir item a lista
    Hemos añadido una barricada para ver que los objetos que entren no
    tengan una calidad mayor a 50

--- add_item.py
+++ add_item.py
@@ -1,5 +1,12 @@
 def add_item(items):
     list_of_items = []
     for item in items:
-        list_of_items.append(item)
+        try:
+            if item.quality > 50:
+                raise ValueError
+        except ValueError:
+            print(item.name + ' no cumple los requisitos para entrar en la tienda.')
+        else:
+            list_of_items.append(item)
+
     return list_of_items
