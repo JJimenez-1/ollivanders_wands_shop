84746edbd69c42a8b17a316cbea7031612d1852e
DanielFernandezR <dfernandez@cifpfbmoll.eu>
2020-01-05 18:25:56 +0100

    refactor: Añadir otra norma a la barricada
    He añadido que ademas no pueda ser la calidad negativa para entrar en la
    tienda

--- add_item.py
+++ add_item.py
@@ -2,7 +2,7 @@ def add_item(items):
     list_of_items = []
     for item in items:
         try:
-            if item.quality > 50:
+            if item.quality > 50 or item.quality < 0:
                 raise ValueError
         except ValueError:
             print(item.name + ' no cumple los requisitos para entrar en la tienda.')
