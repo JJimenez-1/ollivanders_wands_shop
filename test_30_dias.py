2423cbc3225c2f55480c43caf3595b87527204f7
JJimenez-1 <jjimenez@cifpfbmoll.eu>
2020-01-06 18:40:52 +0100

    test: Cambiar caso test 30 dias

--- test_30_dias.py
+++ test_30_dias.py
@@ -6,7 +6,7 @@ from clases.sulfuras import Sulfuras
 from clases.backstagepass import Backstagepass
 from clases.conjured import Conjured
 from clases.gildedrose import Gildedrose
+from main
 
-
-def test_30_dias_modificar_items():
-    
+def test_dia_0():
+    assert inventario.get_items == [+5 Dexterity Vest, 10, 20, Aged Brie, 2, 0, Elixir of the Mongoose, 5, 7, Backstage passes to ...rt, 15, 20, Backstage passes to ...rt, 10, 49, Backstage passes to ...ert, 5, 49, Conjured Mana Cake, 3, 6]
