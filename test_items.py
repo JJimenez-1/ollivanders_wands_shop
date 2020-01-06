417f02d6fed3090fdd1267e9a660d3b1339fa0ef
DanielFernandezR <dfernandez@cifpfbmoll.eu>
2020-01-06 17:04:27 +0100

    fix: Arreglar fallos casos test
    No funcionaban los casos test al cambiar la logica de las clases, ahora
    si funcionan

--- test_items.py
+++ test_items.py
@@ -17,41 +17,48 @@ def test_class_normalitem_works():
 
 def test_class_normalitem_setsellin_negative():
     normal_item = Normalitem("carrot", -1, 8)
-    item_sellin_update = normal_item.set_sellin() and normal_item.update_quality()
-    assert item_sellin_update == "carrot, -2, 6"
+    normal_item.update_quality()
+    normal_item = normal_item.__repr__()
+    assert normal_item == "carrot, -2, 6"
 
 
 def test_class_normalitem_quality_to_zero():
     normal_item = Normalitem("watermelon", -1, 1)
-    item_sellin_update = normal_item.set_sellin() and normal_item.update_quality()
-    assert item_sellin_update == "watermelon, -2, 0"
+    normal_item.update_quality()
+    normal_item = normal_item.__repr__()
+    assert normal_item == "watermelon, -2, 0"
 
 
 def test_class_Agedbrie_works():
     aged_brie = Agedbrie("apple", -25, 48)
-    item_sellin_update = aged_brie.set_sellin() and aged_brie.update_quality()
-    assert item_sellin_update == "apple, -26, 50"
+    aged_brie.update_quality()
+    aged_brie = aged_brie.__repr__()
+    assert aged_brie == "apple, -26, 50"
 
 
 def test_class_Agedbrie_quality_to_50():
     aged_brie = Agedbrie("apple", -25, 49)
-    item_sellin_update = aged_brie.set_sellin() and aged_brie.update_quality()
-    assert item_sellin_update == "apple, -26, 50"
+    aged_brie.update_quality()
+    aged_brie = aged_brie.__repr__()
+    assert aged_brie == "apple, -26, 50"
 
 
 def test_class_Sulfuras_works():
     sulfuras = Sulfuras("Sulfuras", 0, 50)
-    item_update = sulfuras.set_sellin() and sulfuras.update_quality()
-    assert item_update == "Sulfuras, 0, 50"
+    sulfuras.update_quality()
+    sulfuras = sulfuras.__repr__()
+    assert sulfuras == "Sulfuras, 0, 50"
 
 
 def test_class_Backstagepass_works():
     backstage_pass = Backstagepass("Backstagepass", 3, 10)
-    item_update = backstage_pass.set_sellin() and backstage_pass.update_quality()
-    assert item_update == "Backstagepass, 2, 13"
+    backstage_pass.update_quality()
+    backstage_pass = backstage_pass.__repr__()
+    assert backstage_pass == "Backstagepass, 2, 13"
 
 
 def test_class_Conjured_works():
     conjured = Conjured("Conjured", 3, 10)
-    item_update = conjured.set_sellin() and conjured.update_quality()
-    assert item_update == "Conjured, 2, 8"
+    conjured.update_quality()
+    conjured = conjured.__repr__()
+    assert conjured == "Conjured, 2, 8"
