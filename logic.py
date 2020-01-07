b4015dd6bf16ca646b26bd9c7a55c976d4e0f844
JJimenez-1 <jjimenez@cifpfbmoll.eu>
2020-01-07 17:39:21 +0100

    refactor: Cambiar código logic.py
    Hemos cambiado el codigo de logic.py, para hacer las pruebas de los 30
    dias

--- logic.py
+++ logic.py
@@ -5,7 +5,17 @@ def update_days(days_passed):
     updated_inventory = []
     acumulador = 0
     while acumulador < days_passed:
-        for item in items:
-            item.update_quality()
-            updated_inventory.append(item.__repr__())
-    return updated_inventory
+        actualizado = inventario.update_quality()
+        acumulador += 1
+    return actualizado
+
+if __name__ == "__main__":
+    assert update_days(1) == ["+5 Dexterity Vest", 9, 19,
+                         "Elixir of the Mongoose", 4, 6,
+                         "Aged Brie", 1, 1,
+                         "Sulfuras, Hand of Ragnaros", 0, 80,
+                         "Sulfuras, Hand of Ragnaros", -1, 80,
+                         "Backstage passes to a TAFKAL80ETC concert", 14, 21,
+                         "Backstage passes to a TAFKAL80ETC concert", 9, 50,
+                         "Backstage passes to a TAFKAL80ETC concert", 4, 50,
+                         "Conjured Mana Cake", 2, 4]
