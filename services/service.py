92472bc0c53faeed2dfaa8f8f04913b501398c54
DanielFernandezR <dfernandez@cifpfbmoll.eu>
2020-02-07 17:35:14 +0100

    chore: Cambiar estructura de carpetas del proyecto

---
+++ services/service.py
@@ -0,0 +1,13 @@
+class Service():
+
+    def getInventario(tienda):
+        inventario = {}
+        items = tienda.getItems()
+
+        for item in items:
+            inventario[item.name] = item.__dict__
+        return inventario
+
+    def updateQuality(tienda):
+        tienda.update_quality()
+        return Service.getInventario(tienda)
